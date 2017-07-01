c0faa0355663d664238ddf9b07ffd9ace246de11
MohGanji <mohammad-ganji@outlook.com>
2017-07-02 04:22:29 +0430

    added toolbars and text frame

--- Editor.py
+++ Editor.py
@@ -8,7 +8,42 @@ class Editor(QtGui.QMainWindow):
 		QtGui.QMainWindow.__init__(self, parent)
 		self.initUI()
 
+	#-----------------------------
+	# toolbar inits
+	def initToolBar(self):
+		self.toolbar = self.addToolBar("Options")
+		# adds a break so new toolbar will be added under this one.
+		self.addToolBarBreak()
+	
+	def initFormatBar(self):
+		self.formatbar = self.addToolBar("Format")
+
+	def initMenuBar(self):
+		menubar = self.menuBar()
+
+		file = menubar.addMenu("File")
+		edit = menubar.addMenu("Edit")
+		view = menubar.addMenu("View")
+	
+	def initStatusBar(self):
+		statusbar = self.statusBar()
+
+	#-----------------------------
+	# the initial UI
 	def initUI(self):
+		
+		# main text frame
+		self.text = QtGui.QTextEdit(self)
+		self.setCentralWidget(self.text)
+				
+		# toolbars
+		self.initToolBar()
+		self.initFormatBar()
+		self.initMenuBar()
+		self.initStatusBar()
+		
+		# window position and name
 		self.setGeometry(50, 50, 1266, 668)
 		self.setWindowTitle("pyQtEditor")
+
 
